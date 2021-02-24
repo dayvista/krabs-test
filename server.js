7e2e1a34034c4712eae00021840751e5252bb0fb
William Davis <liamdavis@tuta.io>
2021-02-24 10:11:48 -0700

    testing another implementation of next-boost + krabs

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 const express = require("express");
+const server = express();
 const krabs = require("krabs").default;
 const dev = process.env.NODE_ENV !== "production";
 const CachedHandler = require("next-boost").default;
-const next = require("next");
 const args = { dir: ".", dev };
-const app = next(args);
 
 const main = async () => {
   try {
+    const app = require("next")(args);
     await app.prepare();
 
-    const handler = app.getRequestHandler();
+    const devHandler = app.getRequestHandler();
 
-    const cached = await CachedHandler({ handler, args });
-
-    console.log(cached);
-
-    const server = express();
+    const cachedHandler = await CachedHandler({ devHandler, args });
 
     server
       .get("*", (req, res) => {
-        if (req.headers["x-forwarded-proto"] === "http") {
+        if (!dev && req.headers["x-forwarded-proto"] === "http") {
           res.redirect("https://" + req.headers.host + req.url);
         }
 
-        return krabs(req, res, handler, dev ? handler : cached);
+        return krabs(req, res, dev ? devHandler : cachedHandler, app);
       })
       .listen(process.env.PORT || 3000, () =>
         console.log(`Server is ready on port ${process.env.PORT || 3000}.`)
