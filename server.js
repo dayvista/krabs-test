efd231a4d58167edfe1ecc854af51246bb530fec
William Davis <liamdavis@tuta.io>
2021-02-23 20:48:16 -0700

    removed automatic ssl redirect in favor of manual method

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const next = require("next");
 const krabs = require("krabs").default;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
-import sslRedirect from "heroku-ssl-redirect";
 
 const main = async () => {
   try {
@@ -12,15 +11,11 @@ const main = async () => {
     const handle = app.getRequestHandler();
     const server = express();
 
-    // automatic SSL redirect
-    server.use(sslRedirect);
-
     server
       .get("*", (req, res) => {
-        // manual SSL redirect
-        // if (req.headers["x-forwarded-proto"] == "http") {
-        //   res.redirect("https://" + req.headers.host + req.url);
-        // }
+        if (req.headers["x-forwarded-proto"] === "http") {
+          res.redirect("https://" + req.headers.host + req.url);
+        }
 
         return krabs(req, res, handle, app);
       })
