11e1c50385206d8403922bdb3111773c73797b90
William Davis <liamdavis@tuta.io>
2021-02-24 09:29:12 -0700

    testing next-boost

---
+++ .next-boost.js
@@ -0,0 +1,6 @@
+module.exports = {
+  cache: {
+    ttl: 3600,
+    tbd: 3600 * 24 * 5,
+  },
+};
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const next = require("next");
+// const next = require("next");
 const krabs = require("krabs").default;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const CachedHandler = require("next-boost").default;
+// const app = next({ dev });
 
 const main = async () => {
   try {
-    await app.prepare();
+    const init = require("./init").default;
+    const script = require.resolve("./init");
+    const cached = await CachedHandler({ script, args });
 
-    const handle = app.getRequestHandler();
+    const handler = dev ? await init(args) : cached.handler;
+
+    // await app.prepare();
+
+    // const handler = app.getRequestHandler();
     const server = express();
 
     server
@@ -17,7 +24,7 @@ const main = async () => {
           res.redirect("https://" + req.headers.host + req.url);
         }
 
-        return krabs(req, res, handle, app);
+        return krabs(req, res, handler, app);
       })
       .listen(process.env.PORT || 3000, () =>
         console.log(`Server is ready on port ${process.env.PORT || 3000}.`)
